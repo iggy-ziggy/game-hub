83bf5ee749a62f2ca8f521db427603ecf87398fb
Matt Haynes <128410000+iggy-ziggy@users.noreply.github.com>
2024-01-27 17:24:23 -0500

    displaying icons in game card

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,11 +2,19 @@ import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
+export interface Platform {
+    id: number;
+    name: string;
+    slug: string;
+}
+
 // we export this here so that it can be used elsewhere (in the GameCard component)
 export interface Game {
   id: number;
   name: string;
   background_image: string;
+  // see note at bottom about this 'parent_platform' line...
+  parent_platforms: { platform: Platform }[];
 }
 
 interface FetchGamesResponse {
@@ -36,3 +44,14 @@ const useGames = () => {
 };
 
 export default useGames;
+
+
+// NOTES:
+
+    // in the Games interface, we are referencing the parent_platform in a peculiar manner
+    // it's because the API we're using is returning this data in a peculiar manner
+    // instead of returning an array of parent_platform objects, it's rerturning an array of objects with the key of 'platform:' and an object as the value
+    // SO: we can't reference it as 'parent_platform: Platform[]' like we have in other interfaces
+    // we must reference it as 'parent_platform: { platform: Platform }[]'
+    // which is to say: it is an array of objects with a key of 'platform' which is of type 'Platform' (which refers to the interface we made above)
+    // woof...
