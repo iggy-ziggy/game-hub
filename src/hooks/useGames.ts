82a997e8d10cd6628d5419db91f4918942c42693
Matt Haynes <128410000+iggy-ziggy@users.noreply.github.com>
2024-02-20 12:40:05 -0500

    refactor - removed duplicate interfaces

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,12 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
 import apiClient from "../services/api-client";
 import { FetchResponse } from "../services/api-client";
-
-export interface Platform {
-  id: number;
-  name: string;
-  slug: string;
-}
+import { Platform } from "./usePlatforms";
 
 // we export this here so that it can be used elsewhere (in the GameCard component)
 export interface Game {
