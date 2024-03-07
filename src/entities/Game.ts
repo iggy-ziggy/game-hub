473543f5ef422b79a94cd9380b0892e2c0e7288a
Matt Haynes <128410000+iggy-ziggy@users.noreply.github.com>
2024-03-07 14:00:53 -0500

    added game attributes component

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,4 +1,6 @@
+import { Genre } from "./Genre";
 import { Platform } from "./Platform";
+import { Publisher } from "./Publisher";
 
 // we export this here so that it can be used elsewhere (in the GameCard component)
 
@@ -6,6 +8,8 @@ export interface Game {
   id: number;
   name: string;
   slug: string;
+  genres: Genre[];
+  publishers: Publisher[];
   description_raw: string;
   background_image: string;
   // see note at bottom about this 'parent_platform' line...
