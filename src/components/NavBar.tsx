35eaeb1a7362bd3dc35c1b0ddea646712a42fbab
Matt Haynes <128410000+iggy-ziggy@users.noreply.github.com>
2024-03-08 12:23:36 -0500

    fixed issue with navbar search

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,26 +1,28 @@
-import { HStack, Image } from '@chakra-ui/react'
-import logo from '../assets/logo.webp';
-import ColorModeSwitch from './ColorModeSwitch';
-import SearchInput from './SearchInput';
-import useGameQueryStore from '../store';
+import { HStack, Image } from "@chakra-ui/react";
+import logo from "../assets/logo.webp";
+import ColorModeSwitch from "./ColorModeSwitch";
+import SearchInput from "./SearchInput";
+import useGameQueryStore from "../store";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
-  const {setSearchText} = useGameQueryStore()
+  const { setSearchText } = useGameQueryStore();
   return (
-    <HStack padding='10px'>
-        <Image src={logo} boxSize='60px'/>
-        <SearchInput />
-        <ColorModeSwitch />
+    <HStack padding="10px">
+      <Link to="/" >
+        <Image src={logo} boxSize="60px" objectFit='cover'/>
+      </Link>
+      <SearchInput />
+      <ColorModeSwitch />
     </HStack>
-  )
-}
+  );
+};
 
-export default NavBar
-
+export default NavBar;
 
 // Notes:
-    // HStack is a Chakra UI component - 'horizontal stack'
-    // Image is also a Chakra UI component
-        // we cannot reference the src by its relative path - it must be imported like any other component then referenced
-            // boxSize is, obviously, part of Chakra UI
-        // Text is, you guessed it, another Chakra UI component
+// HStack is a Chakra UI component - 'horizontal stack'
+// Image is also a Chakra UI component
+// we cannot reference the src by its relative path - it must be imported like any other component then referenced
+// boxSize is, obviously, part of Chakra UI
+// Text is, you guessed it, another Chakra UI component
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,4 +1,11 @@
-import { Box, Heading, SimpleGrid, Spinner, Text } from "@chakra-ui/react";
+import {
+  Box,
+  GridItem,
+  Heading,
+  SimpleGrid,
+  Spinner,
+  Text,
+} from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import CriticScore from "../components/CriticScore";
 import DefinitionItem from "../components/DefinitionItem";
@@ -17,13 +24,17 @@ const GameDetailPage = () => {
   if (error || !game) throw error;
 
   return (
-    <>
-      <Heading>{game?.name}</Heading>
-      <ExpandableText>{game?.description_raw}</ExpandableText>
-      <GameAttributes game={game} />
-      <GameTrailer gameId={game.id} />
-      <GameScreenshots gameId={game.id} />
-    </>
+      <SimpleGrid columns={{ sm: 1, md: 2 }} spacing={5}>
+        <GridItem>
+          <Heading>{game?.name}</Heading>
+          <ExpandableText>{game?.description_raw}</ExpandableText>
+          <GameAttributes game={game} />
+        </GridItem>
+        <GridItem>
+          <GameTrailer gameId={game.id} />
+          <GameScreenshots gameId={game.id} />
+        </GridItem>
+      </SimpleGrid>
   );
 };
 
